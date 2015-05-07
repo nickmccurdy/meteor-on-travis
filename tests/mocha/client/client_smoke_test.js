75d2100bcfae1ea1a6b90e406d6bce71eec6e17d
Nicolas McCurdy <thenickperson@gmail.com>
2015-05-06 21:28:44 -0400

    Rename existing tests to include "smoke test" in their names

--- tests/mocha/client/client_smoke_test.js
+++ tests/mocha/client/client_smoke_test.js
@@ -1,6 +1,6 @@
 if (MochaWeb) {
   MochaWeb.testOnly(function () {
-    describe("client side test suite", function () {
+    describe("client side smoke test", function () {
       it("runs successfully", function () {
         chai.assert(true);
       });
--- tests/mocha/server/server_smoke_test.js
+++ tests/mocha/server/server_smoke_test.js
@@ -1,6 +1,6 @@
 if (MochaWeb) {
   MochaWeb.testOnly(function () {
-    describe("server side test suite", function () {
+    describe("server side smoke test", function () {
       it("runs successfully", function () {
         chai.assert(true);
       });
